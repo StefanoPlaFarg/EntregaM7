8c4b362532dee2fc714db8336c193490bcfd02ec
Stefano <stefano.pola.fargas@gmail.com>
2020-12-03 16:23:42 +0100

    Creacio constructors User

--- src/domain/User.java
+++ src/domain/User.java
@@ -3,10 +3,32 @@
  */
 package domain;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author stefano
  *
  */
 public class User {
+	
+	private String name;
+	private String surname;
+	private String password;
+	Timestamp timeStamp;
+	
+	
+	public User () {
+		
+		
+	}
+	
+   public User (String name, String surname, String password, Timestamp timeStamp  ) {
+	   this.name=name;
+	   this.surname=surname;
+	   this.password=password;
+	   this.timeStamp=timeStamp;
+	}
 
 }

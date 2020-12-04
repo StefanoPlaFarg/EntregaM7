f67eca4c4d3b7f689a579ab846a0c68ac4b969d5
Stefano <stefano.pola.fargas@gmail.com>
2020-12-04 17:37:15 +0100

    Creacio de getAllVideos a Controller

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -8,7 +8,9 @@ import persistence.UserRepository;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 
+
 import domain.User;
+import  domain.Video;
 
 
 /**
@@ -69,7 +71,63 @@ public class Controller {
 
 	}
 	
-	public void addUrl(String name, String surname, String password, String title, String url) throws Exception {
+	 public boolean containsVideo (String name, String surname, String password, String title) throws Exception {
+			
+			if (name.equals(""))
+				throw new Exception("Falta introduir el nom");
+			if (surname.equals(""))
+				throw new Exception("Falta introduir el cognom");
+			if (password.equals(""))
+				throw new Exception("Falta introduir el password");
+			if (title.equals(""))
+				throw new Exception("Falta introduir el titol");
+			
+			
+			return repository.getUser(name,surname, password).containsVideo(title);
+		}
+	 
+	 
+	 
+	
+	 public void updateTitleVideo (String name, String surname, String password, String oldTitle, String newTitle) throws Exception {
+			
+			if (name.equals(""))
+				throw new Exception("Falta introduir el nom");
+			if (surname.equals(""))
+				throw new Exception("Falta introduir el cognom");
+			if (password.equals(""))
+				throw new Exception("Falta introduir el password");
+			if (oldTitle.equals(""))
+				throw new Exception("Falta introduir el titol");
+			
+			if (repository.getUser(name,surname, password).containsVideo(oldTitle)) {
+				repository.getUser(name,surname, password).getVideoByTitle(oldTitle).setTitle(newTitle);
+			}
+			
+			
+		}
+	
+	
+	 public String getUrlVideo (String name, String surname, String password, String title) throws Exception {
+			
+			if (name.equals(""))
+				throw new Exception("Falta introduir el nom");
+			if (surname.equals(""))
+				throw new Exception("Falta introduir el cognom");
+			if (password.equals(""))
+				throw new Exception("Falta introduir el password");
+			if (title.equals(""))
+				throw new Exception("Falta introduir el titol");
+			
+			
+			return repository.getUser(name,surname, password).getVideoByTitle(title).getUrl();
+		}
+	
+	
+	
+	
+	
+	public void updateUrlVideo(String name, String surname, String password, String title, String url) throws Exception {
 		if (name.equals(""))
 			throw new Exception("Falta introduir el nom");
 		if (surname.equals(""))
@@ -100,6 +158,40 @@ public class Controller {
 		repository.getUser(name, surname, password).getVideoByTitle(title).addTag(tag);
 
 	}
+	
+	
+	
+	
+	public ArrayList<String>  getAllTags(String name, String surname, String password, String title) throws Exception {
+		if (name.equals(""))
+			throw new Exception("Falta introduir el nom");
+		if (surname.equals(""))
+			throw new Exception("Falta introduir el cognom");
+		if (password.equals(""))
+			throw new Exception("Falta introduir el password");
+		if (title.equals(""))
+			throw new Exception("Falta introduir el titol");
+		
+
+		return repository.getUser(name, surname, password).getVideoByTitle(title).getTagsList();
+
+	}
+	
+	public String  getAllVideos(String name, String surname, String password) throws Exception {
+		if (name.equals(""))
+			throw new Exception("Falta introduir el nom");
+		if (surname.equals(""))
+			throw new Exception("Falta introduir el cognom");
+		if (password.equals(""))
+			throw new Exception("Falta introduir el password");
+		
+		return repository.getUser(name, surname, password).getAllTitlesOfVideos();
+
+		
+
+	}
+	
+	
 		
 }
 	

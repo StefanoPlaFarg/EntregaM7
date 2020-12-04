637a7c934884fc67f70b9c06d6babfe4427464cf
Stefano <stefano.pola.fargas@gmail.com>
2020-12-04 16:42:25 +0100

    Creacio de addVideo, addUrl, addTag i mod. de getUser de Controller

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -25,15 +25,18 @@ public class Controller {
 		
 	}
 	
-	public void addUser (String name, String surname, String password, Timestamp timeStamp ) {
+	public void addUser (String name, String surname, String password, Timestamp timeStamp )throws Exception {
 		
-		try {
+		if (name.equals(""))
+			throw new Exception("Falta introduir el nom");
+		if (surname.equals(""))
+			throw new Exception("Falta introduir el cognom");
+		if (password.equals(""))
+			throw new Exception("Falta introduir el password");
+		
+		
 		repository.addUser(new User(name,surname, password,  timeStamp));
-		}catch (Exception e){
-			e.printStackTrace();
-		}finally {
-			
-		}
+		
 		
 	}
 	
@@ -49,21 +52,60 @@ public class Controller {
 			throw new Exception("Falta introduir el password");
 		
 		
-		for (User user:repository.getAllUsers()) {
-			
-			if (user.getName().equals(name)&& user.getSurname().equals(surname)&& user.getPassword().equals(password)) {
-				return user;
-			}
-		}
-		
-		return null;
+		return repository.getUser(name,surname, password);
 	}
 	
-	
-	
-		
-		
+	public void addVideo(String name, String surname, String password, String title) throws Exception {
+		if (name.equals(""))
+			throw new Exception("Falta introduir el nom");
+		if (surname.equals(""))
+			throw new Exception("Falta introduir el cognom");
+		if (password.equals(""))
+			throw new Exception("Falta introduir el password");
+		if (title.equals(""))
+			throw new Exception("Falta introduir el titol");
+
+		repository.getUser(name, surname, password).addVideo(title);
+
 	}
+	
+	public void addUrl(String name, String surname, String password, String title, String url) throws Exception {
+		if (name.equals(""))
+			throw new Exception("Falta introduir el nom");
+		if (surname.equals(""))
+			throw new Exception("Falta introduir el cognom");
+		if (password.equals(""))
+			throw new Exception("Falta introduir el password");
+		if (title.equals(""))
+			throw new Exception("Falta introduir el titol");
+		if (url.equals(""))
+			throw new Exception("Falta introduir l'url");
+
+		repository.getUser(name, surname, password).getVideoByTitle(title).setUrl(url);
+
+	}
+	
+	public void addTag(String name, String surname, String password, String title, String tag) throws Exception {
+		if (name.equals(""))
+			throw new Exception("Falta introduir el nom");
+		if (surname.equals(""))
+			throw new Exception("Falta introduir el cognom");
+		if (password.equals(""))
+			throw new Exception("Falta introduir el password");
+		if (title.equals(""))
+			throw new Exception("Falta introduir el titol");
+		if (tag.equals(""))
+			throw new Exception("Falta introduir l'url");
+
+		repository.getUser(name, surname, password).getVideoByTitle(title).addTag(tag);
+
+	}
+		
+}
+	
+		
+		
+	
 	
 	
 	
